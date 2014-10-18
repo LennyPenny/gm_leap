6d931cf71c12249084de852c6c6f4d53d8c37cc9
LennyPenny <l.bernhardt@live.de>
2014-10-18 17:53:23 +0200

    init module

---
+++ premake5.lua
@@ -0,0 +1,58 @@
+assert(os.getenv("LEAP_SDK"), "LEAP_SDK environmental variable not set")
+
+local LEAP_SDK = os.getenv("LEAP_SDK")
+
+solution "gm_stringtable"
+	language "C++"
+	location "project"
+	targetdir "bin"
+
+	flags { "StaticRuntime" }
+
+	includedirs {
+		"include",
+		LEAP_SDK .. "/include"
+	}
+
+	libdirs {
+		LEAP_SDK .. "/lib/x86"
+	}
+	
+
+	configurations {
+		"Debug",
+		"Release"
+	}
+
+	configuration "Debug"
+		flags { "symbols" }
+
+		links {
+			"Leapd.lib"
+		}
+
+	configuration "Release"
+		optimize "On"
+		
+		links {
+			"Leap.lib"
+		}
+		
+
+	project "gmcl_leap"
+		kind "SharedLib"
+		
+		files {
+			"src/**.cpp",
+			"src/**.h"
+		}
+
+		defines { "GMMODULE" }
+
+		if os.is "windows" then
+			targetsuffix "_win32"
+		elseif os.is "linux" then
+			targetsuffix "_linux"
+		elseif os.is "macosx" then
+			targetsuffix "_osx"
+		end
---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include "GarrysMod\Lua\Interface.h"
+
+#include "Leap.h"
+
+using namespace GarrysMod;
+using namespace Leap;
+
+GMOD_MODULE_OPEN() {
+	return 0;
+}
+
+GMOD_MODULE_CLOSE() {
+	return 0;
+}
