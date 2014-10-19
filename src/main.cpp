f15c4c2417de68ba0213b9ac6f98677bb1a4daf9
Jvs <jvstheluacoder@gmail.com>
2014-10-19 18:43:09 +0200

    leap.DeserializeFrame( string )

--- src/main.cpp
+++ src/main.cpp
@@ -11,23 +11,23 @@ using namespace Leap;
 using namespace GModLeap;
 
 Controller *controller = nullptr;
-void *fake_heap[sizeof(Controller)];
+void *fake_heap[ sizeof( Controller ) ];
 
-int leap_IsConnected(lua_State *state) {
-	LUA->PushBool(controller->isConnected());
+int leap_IsConnected( lua_State *state ) {
+	LUA->PushBool( controller->isConnected() );
 
 	return 1;
 }
 
-int leap_Frame(lua_State *state) {
+int leap_Frame( lua_State *state ) {
 	
 	//Jvs:	I'm using the copy constructor due to the controller cleaning up the history after a few ticks
 	//		we're gonna have to add a __gc method to the frame, but this also stops the crashing bullshit
 	
 	Frame *frame;
 	
-	if (LUA->Top() > 0) {
-		frame = new Frame( controller->frame(LUA->CheckNumber()) );
+	if ( LUA->Top() > 0 ) {
+		frame = new Frame( controller->frame( LUA->CheckNumber() ) );
 	}
 	else {
 		frame = new Frame( controller->frame() );
@@ -38,11 +38,28 @@ int leap_Frame(lua_State *state) {
 	return 1;
 }
 
+int leap_DeserializeFrame( lua_State *state ) {
+	
+	LUA->CheckType( 1 , Lua::Type::STRING );
+	
+	unsigned int serializedframelen = 0;
+	const char *serializedframe = LUA->GetString( 1 , &serializedframelen );
+
+	std::string stdserializedframe( serializedframe , serializedframelen );
+
+	Frame *newframe = new Frame();
+	newframe->deserialize( stdserializedframe );
+
+	LeapFrame::Push( state , newframe );
+	
+	return 1;
+}
+
 
 
 GMOD_MODULE_OPEN() {
 	
-	controller = new (fake_heap) Controller;
+	controller = new ( fake_heap ) Controller;
 
 	LUA->PushSpecial( Lua::SPECIAL_GLOB );
 
@@ -54,8 +71,10 @@ GMOD_MODULE_OPEN() {
 			LUA->PushCFunction( leap_Frame );
 			LUA->SetField( -2 , "Frame" );
 
-		LUA->SetField( -2 , "leap" );
+			LUA->PushCFunction( leap_DeserializeFrame );
+			LUA->SetField( -2 , "DeserializeFrame" );
 
+		LUA->SetField( -2 , "leap" );
 
 		LeapFrame::DefineMeta( state );
 
