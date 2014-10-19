2e4b50c3fdea49e50a55e49d221bf9734dbd8af3
Jvs <jvstheluacoder@gmail.com>
2014-10-19 12:03:03 +0200

    Fixed crashing
    We're gonna have to add a garbage collect method for the frame.

--- src/main.cpp
+++ src/main.cpp
@@ -15,21 +15,25 @@ int leap_IsConnected(lua_State *state) {
 }
 
 int leap_Frame(lua_State *state) {
-	Frame frame;
+	
+	//Jvs:	I'm using the copy constructor due to the controller cleaning up the history after a few ticks
+	//		we're gonna have to add a __gc method to the frame, but this also stops the crashing bullshit
 
+	Frame * frame;
+	
 	if (LUA->Top() > 0) {
-		frame = controller->frame(LUA->CheckNumber());
+		frame = new Frame( controller->frame(LUA->CheckNumber()) );
 	}
 	else {
-		frame = controller->frame();
+		frame = new Frame( controller->frame() );
 	}
 
 	Lua::UserData* ud = (Lua::UserData*)LUA->NewUserdata(sizeof(Lua::UserData));
-	ud->data = &frame;
-	ud->type = Lua::Type::USERDATA;
+	ud->data = frame;
+	ud->type = 99;
 
-	LUA->CreateMetaTableType("LeapFrame", Lua::Type::USERDATA);
-	LUA->SetMetaTable(-2);
+	LUA->CreateMetaTableType("LeapFrame", 99 );
+	LUA->SetMetaTable( -2 );
 
 	return 1;
 }
@@ -39,21 +43,51 @@ int leap_Frame(lua_State *state) {
 
 int LeapFrame_tostring(lua_State* state) {
 	Lua::UserData* ud = (Lua::UserData*)LUA->GetUserdata();
+	
+	if ( !ud ) return 0;
+
 	Frame* frame = (Frame*)ud->data;
+	if ( !frame || !frame->isValid() )
+	{
+		return 0;
+	}
 
 	LUA->PushString(frame->toString().c_str());
-
 	return 1;
 }
 
 int LeapFrame_Serialize(lua_State* state) {
+	
 	Lua::UserData* ud = (Lua::UserData*)LUA->GetUserdata();
+	
+	if ( !ud ) return 0;
+
 	Frame* frame = (Frame*)ud->data;
+	if ( !frame || !frame->isValid() )
+	{
+		return 0;
+	}
 
 	LUA->PushString(frame->serialize().c_str());
-
 	return 1;
 }
+
+int LeapFrame_IsValid(lua_State* state) {
+	
+	Lua::UserData* ud = (Lua::UserData*)LUA->GetUserdata();
+	
+	if ( !ud ) return 0;
+
+	Frame* frame = (Frame*)ud->data;
+	if ( !frame )
+	{
+		return 0;
+	}
+
+	LUA->PushBool( frame->isValid() );
+	return 1;
+}
+
 
 GMOD_MODULE_OPEN() {
 	
@@ -83,6 +117,10 @@ GMOD_MODULE_OPEN() {
 
 			LUA->PushCFunction(LeapFrame_Serialize);
 			LUA->SetField(-2, "Serialize");
+
+			LUA->PushCFunction(LeapFrame_IsValid);
+			LUA->SetField(-2, "IsValid");
+
 		
 		LUA->Pop();
 
