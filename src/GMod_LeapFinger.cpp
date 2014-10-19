e3df03b4824e6feb15291187bd2c25ff13bfbcc3
Jvs <jvstheluacoder@gmail.com>
2014-10-19 16:59:34 +0200

    Forgot finger.cpp

--- src/GMod_LeapFinger.cpp
+++ src/GMod_LeapFinger.cpp
@@ -9,50 +9,50 @@ using namespace GModLeap;
 int LeapFinger::TYPE = 101; //random
 
 
-void LeapFinger::DefineMeta(lua_State * state)
+void LeapFinger::DefineMeta( lua_State *state )
 {
-	LUA->CreateMetaTableType("LeapFinger", LeapFinger::TYPE);
-		LUA->Push(-1);
-		LUA->SetField(-2, "__index");
+	LUA->CreateMetaTableType( "LeapFinger" , LeapFinger::TYPE );
+		LUA->Push( -1 );
+		LUA->SetField( -2 , "__index" );
 
-		LUA->PushCFunction(LeapFinger::tostring);
-		LUA->SetField(-2, "__tostring");
+		LUA->PushCFunction( LeapFinger::tostring );
+		LUA->SetField( -2 , "__tostring" );
 
-		LUA->PushString("LeapFinger");
-		LUA->SetField(-2, "__type");
+		LUA->PushString( "LeapFinger" );
+		LUA->SetField( -2 , "__type" );
 
-		LUA->PushCFunction(LeapFinger::IsValid);
-		LUA->SetField(-2, "IsValid");
+		LUA->PushCFunction( LeapFinger::IsValid );
+		LUA->SetField( -2 , "IsValid" );
 	LUA->Pop();
 }
 
-Finger *LeapFinger::Get(lua_State *state, int pos)
+Finger *LeapFinger::Get( lua_State *state , int pos )
 {
-	if (!LUA->IsType(pos, LeapFinger::TYPE)) return nullptr;
+	if ( !LUA->IsType( pos , LeapFinger::TYPE ) ) return nullptr;
 
-	Lua::UserData* ud = (Lua::UserData*)LUA->GetUserdata();
+	Lua::UserData *ud = ( Lua::UserData * ) LUA->GetUserdata();
 
-	if (!ud) return nullptr;
+	if ( !ud ) return nullptr;
 
-	Finger* finger = (Finger*)ud->data;
-	if (!finger) return nullptr;
+	Finger *finger = ( Finger * ) ud->data;
+	if ( !finger ) return nullptr;
 
 	return finger;
 }
 
-void LeapFinger::Push(lua_State * state, Leap::Frame *finger)
+void LeapFinger::Push( lua_State * state, Leap::Finger *finger )
 {
-	if (!finger) return;
+	if ( !finger ) return;
 
-	Lua::UserData* ud = (Lua::UserData*)LUA->NewUserdata(sizeof(Lua::UserData));
+	Lua::UserData *ud = ( Lua::UserData * )LUA->NewUserdata( sizeof( Lua::UserData ) );
 	ud->data = finger;
 	ud->type = LeapFinger::TYPE;
 
-	LUA->CreateMetaTableType("LeapFinger", LeapFinger::TYPE);
-	LUA->SetMetaTable(-2);
+	LUA->CreateMetaTableType( "LeapFinger" , LeapFinger::TYPE );
+	LUA->SetMetaTable( -2 );
 }
 
-int LeapFinger::tostring(lua_State* state) {
+int LeapFinger::tostring( lua_State *state) {
 	Finger *finger = Get( state );
 	if ( !finger ) return 0;
 
@@ -62,7 +62,7 @@ int LeapFinger::tostring(lua_State* state) {
 }
 
 
-int LeapFinger::IsValid(lua_State *state) {
+int LeapFinger::IsValid( lua_State *state ) {
 	Finger *finger = Get( state );
 	if ( !finger ) return 0;
 
